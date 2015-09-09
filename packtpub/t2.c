d6844768d679ce7b6bd349916ce7af8f4044cb76
superizer <superizer.tone@gmail.com>
2015-09-09 08:53:21 +0000

    add remove_h2 function, write time and book name to file

--- packtpub/t2.c
+++ packtpub/t2.c
@@ -2,6 +2,12 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#include <sys/time.h>
+#include <time.h>
+
+#define CMD "curl -s  https://www.packtpub.com/packt/offers/free-learning > free-learning.txt"
+#define FILENAME "free-learning.txt"
+#define RESULTFILE "booklist.txt"
 
 char *trimwhitespace(char *str)
 {
@@ -22,33 +28,51 @@ char *trimwhitespace(char *str)
   return str;
 }
 
+char *remove_h2(char *str){
+  char *pch = strstr(str,"</h2>");
+  if(pch != NULL)
+    *pch = 0;
+  return str;
+}
+
 int main(int argc, char **argv){
-  FILE *f;
+  FILE *fo,*fw;
   char buf[2048];
   int count = 0;
 
-  f = fopen(argv[1], "r");
-  if(f == (FILE *)NULL){
+  system(CMD);
+
+
+  fo = fopen(FILENAME, "r");
+  if(fo == (FILE *)NULL){
     perror("fopen");
     exit(-1);
   }
-  
+
+  fw = fopen(RESULTFILE, "a");
+
+  struct timeval tv;
+  gettimeofday(&tv, NULL);
+  char* time = ctime(&tv.tv_sec);
+  time[strlen(time)-1] = 0;
+  //printf("%s : ", time);
+  fprintf(fw,"%s : ", time);
+
   while(1){
-    char *s = fgets(buf, sizeof(buf), f);
+    char *s = fgets(buf, sizeof(buf), fo);
+
     if(s == (char *)NULL)
       break;
-    if(++count == 600){
-      
-      // Remove </h2>
-      char *end = buf+ strlen(buf) - 6;
-      *end = 0;
 
-      printf("%s\n",trimwhitespace(buf));
+    if(++count == 600){
+      //printf("%s\n",remove_h2(trimwhitespace(buf)));
+      fprintf(fw,"%s\n",remove_h2(trimwhitespace(buf)));
     }
     //printf("%d: %s",++count,buf);
   }
 
-  fclose(f);
+  fclose(fo);
+  fclose(fw);
 
 
   return 0;
